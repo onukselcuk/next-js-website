739840562e8495cdd6b464f9702043c94ef77de4
Selcuk <devonukselcuk@gmail.com>
2019-11-29 19:45:03 +0300

    small change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (dev === false) {
 	sitemap({
 		baseUrl: "https://www.istanbulsmilecenter.co",
 		pagesDirectory: __dirname + "\\pages",
-		targetDirectory: __dirname + "/.next/static/",
+		targetDirectory: __dirname + "\\.next\\static",
 		nextConfigPath: __dirname + "\\next.config.js",
 		ignoredExtensions: [ "png", "jpg" ]
 	});
