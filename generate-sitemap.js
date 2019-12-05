fe4389a517485befded99bdc5fb01538c3e25aff
Selcuk <devonukselcuk@gmail.com>
2019-12-05 18:06:06 +0300

    change sitemap generator file

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -2,8 +2,8 @@ const sitemap = require("nextjs-sitemap-generator");
 
 sitemap({
 	baseUrl: "https://www.istanbulsmilecenter.co",
-	pagesDirectory: __dirname + "\\pages",
-	targetDirectory: __dirname + "\\.next\\static",
-	nextConfigPath: __dirname + "\\next.config.js",
+	pagesDirectory: __dirname + "/pages",
+	targetDirectory: __dirname + "/.next/static",
+	nextConfigPath: __dirname + "/next.config.js",
 	ignoredExtensions: [ "png", "jpg" ]
 });
