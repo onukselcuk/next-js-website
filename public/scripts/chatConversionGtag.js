b4e779845cee2053b770d7e3fe5b5f6e4e7aad3d
Selcuk <devonukselcuk@gmail.com>
2019-12-13 21:02:26 +0300

    change chat event name gtm

--- public/scripts/chatConversionGtag.js
+++ public/scripts/chatConversionGtag.js
@@ -1,6 +1,6 @@
 var Tawk_API = Tawk_API || {};
 Tawk_API.onChatStarted = function () {
 	if (dataLayer) {
-		dataLayer.push({ event: "chat-started" });
+		dataLayer.push({ event: "chatStarted" });
 	}
 };
