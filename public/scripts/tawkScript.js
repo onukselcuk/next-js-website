aa9eba5d59d4309e096705514c3ee8bcf8eb75af
Selcuk <devonukselcuk@gmail.com>
2019-12-19 16:52:43 +0300

    change tawkscript cdn option to original server

--- public/scripts/tawkScript.js
+++ public/scripts/tawkScript.js
@@ -4,8 +4,8 @@ var Tawk_API = Tawk_API || {},
 	var s1 = document.createElement("script"),
 		s0 = document.getElementsByTagName("script")[0];
 	s1.async = true;
-	// s1.src = "https://embed.tawk.to/5dced60d43be710e1d1d803b/default";
-	s1.src = "https://dvmbzs0q2ij2d.cloudfront.net/js/tawkLocal.js";
+	s1.src = "https://embed.tawk.to/5dced60d43be710e1d1d803b/default";
+	//s1.src = "https://dvmbzs0q2ij2d.cloudfront.net/js/tawkLocal.js";
 	s1.charset = "UTF-8";
 	// s1.setAttribute("crossorigin", "*");
 	s0.parentNode.insertBefore(s1, s0);
